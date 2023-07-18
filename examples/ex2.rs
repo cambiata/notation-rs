4fc389db73d78b6375259b3f2d8312707c5978de
jonasnys@gmail.com <jonasnys@gmail.com>
2023-07-18 23:27:51 +0200

    update

--- examples/ex2.rs
+++ examples/ex2.rs
@@ -6,7 +6,8 @@
 #![allow(clippy::uninlined_format_args)]
 #![allow(clippy::useless_format)]
 
-use std::borrow::BorrowMut;
+mod testdata;
+use crate::testdata::*;
 
 use graphics::{glyphs::ebgaramond::*, prelude::*};
 use notation_rs::{
@@ -14,160 +15,8 @@ use notation_rs::{
 };
 use render_notation::render::dev::*;
 
-fn r10() -> Vec<NRect> {
-    vec![NRect::new(0.0, 0.0, 10.0, 10.0)]
-}
-
-fn r20() -> Vec<NRect> {
-    vec![NRect::new(0.0, 0.0, 10.0, 20.0)]
-}
-
-// trait Col {}
-
-#[derive(Debug)]
-pub struct MItem {
-    pub rects: Vec<NRect>,
-    pub duration: Duration,
-    pub position: Position,
-    pub col_idx: usize,
-    pub row_idx: usize,
-}
-
-impl MItem {
-    pub fn new(rects: Vec<NRect>, dur: Duration) -> Self {
-        Self {
-            rects,
-            duration: dur,
-            position: 0,
-            col_idx: 0,
-            row_idx: 0,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct MCol {
-    pub duration: Duration,
-    pub rowitems: Vec<Option<Rc<RefCell<MItem>>>>,
-    pub spacing: f32,
-    pub col_idx: usize,
-}
-
-impl MCol {
-    pub fn new(rowitems: Vec<Option<Rc<RefCell<MItem>>>>, duration: Option<Duration>) -> Self {
-        Self {
-            rowitems,
-            duration: duration.unwrap_or(0),
-            spacing: 0.0,
-            col_idx: 0,
-        }
-    }
-}
-
-// impl Col for MCol {}
-
-#[derive(Debug)]
-pub struct MMatrix {
-    pub colitems: Vec<Rc<RefCell<MCol>>>,
-    pub rowitems: Vec<Vec<Option<Rc<RefCell<MItem>>>>>,
-}
-
-impl MMatrix {
-    pub fn new(colitems: Vec<Rc<RefCell<MCol>>>) -> Self {
-        let row_count = &colitems[0].borrow().rowitems.len();
-
-        let mut rowitems: Vec<Vec<Option<Rc<RefCell<MItem>>>>> = vec![vec![]; *row_count];
-
-        let firstcol = &colitems[0];
-        for item in firstcol.borrow().rowitems.iter() {
-            if item.is_none() {
-                panic!("firstcol has None - shouldn't have!");
-            }
-        }
-
-        let mut colidx = 0;
-        for col in &colitems {
-            let col: &RefCell<MCol> = col;
-
-            // set column index
-            col.borrow_mut().col_idx = colidx;
-
-            // check for rows integrity
-            if col.borrow().rowitems.len() != *row_count {
-                panic!("part_count mismatch");
-            }
-
-            let mut rowidx = 0;
-            for row in col.borrow_mut().rowitems.iter_mut() {
-                // set rowidx and colidx for item
-                if let Some(row) = row {
-                    let row: &RefCell<MItem> = row;
-                    row.borrow_mut().col_idx = colidx;
-                    row.borrow_mut().row_idx = rowidx;
-                }
-
-                // add cloned item to rowitems
-                let rowitem = if let Some(row) = row {
-                    let item = row.clone();
-                    Some(row.clone())
-                } else {
-                    None
-                };
-                rowitems[rowidx].push(rowitem);
-
-                rowidx += 1;
-            }
-
-            colidx += 1;
-        }
-        Self { colitems, rowitems }
-    }
-}
-
 fn main() {
-    let col0 = MCol::new(
-        vec![
-            Some(Rc::new(RefCell::new(MItem::new(r20(), 0)))),
-            Some(Rc::new(RefCell::new(MItem::new(r20(), 0)))),
-            Some(Rc::new(RefCell::new(MItem::new(r20(), 0)))),
-            //
-        ],
-        None,
-    );
-    let col1 = MCol::new(
-        vec![
-            Some(Rc::new(RefCell::new(MItem::new(r10(), NV1)))),
-            Some(Rc::new(RefCell::new(MItem::new(r10(), NV4)))),
-            Some(Rc::new(RefCell::new(MItem::new(r10(), NV2)))),
-            //
-        ],
-        Some(NV4),
-    );
-    let col2 = MCol::new(
-        vec![
-            None, //
-            Some(Rc::new(RefCell::new(MItem::new(r10(), NV2DOT)))),
-            None, //
-        ],
-        Some(NV4),
-    );
-    let col3 = MCol::new(
-        vec![
-            None, //
-            None, //
-            Some(Rc::new(RefCell::new(MItem::new(r10(), NV2)))),
-        ],
-        Some(NV2),
-    );
-
-    let matrix = MMatrix::new(vec![
-        Rc::new(RefCell::new(col0)),
-        Rc::new(RefCell::new(col1)),
-        Rc::new(RefCell::new(col2)),
-        Rc::new(RefCell::new(col3)),
-    ]);
-
-    //-----------------------------------------------------------------------
+    let matrix = matrix_test1();
 
     let mut items = GraphicItems::new();
 
@@ -182,7 +31,6 @@ fn main() {
                 let item = row.borrow();
                 let rects = &item.rects;
                 for rect in rects {
-                    // let graphic_rect = nrect2rect(*rect, Strokestyle(1., Blue), NoFill);
                     let color = if col.duration == 0 { "orange" } else { "blue" };
                     let nrect = NRectExt::new(
                         rect.move_rect(x, y),
@@ -193,11 +41,11 @@ fn main() {
                 }
             } else {
                 let rect = NRect::new(x, y, 10.0, 10.0);
-                let nrect = NRectExt::new(rect, NRectType::Dev(true, "red".to_string()));
+                let nrect = NRectExt::new(rect, NRectType::Dev(true, "gray".to_string()));
                 let graphic_item = next2graphic(&nrect).unwrap();
                 items.push(graphic_item);
             }
-            y += 50.0;
+            y += 30.0;
         }
         x += 30.0;
     }
---
+++ examples/testdata/mod.rs
@@ -0,0 +1,163 @@
+pub fn matrix_test1() -> RMatrix {
+    let col0 = RCol::new(
+        vec![
+            Some(Rc::new(RefCell::new(RItem::new(r20(), 0)))),
+            Some(Rc::new(RefCell::new(RItem::new(r20(), 0)))),
+            Some(Rc::new(RefCell::new(RItem::new(r20(), 0)))),
+            //
+        ],
+        None,
+    );
+    let col1 = RCol::new(
+        vec![
+            Some(Rc::new(RefCell::new(RItem::new(r10(), NV1)))),
+            Some(Rc::new(RefCell::new(RItem::new(r10(), NV4)))),
+            Some(Rc::new(RefCell::new(RItem::new(r10(), NV2)))),
+            //
+        ],
+        Some(NV4),
+    );
+    let col2 = RCol::new(
+        vec![
+            None, //
+            Some(Rc::new(RefCell::new(RItem::new(r10(), NV2DOT)))),
+            None, //
+        ],
+        Some(NV4),
+    );
+    let col3 = RCol::new(
+        vec![
+            None, //
+            None, //
+            Some(Rc::new(RefCell::new(RItem::new(r10(), NV2)))),
+        ],
+        Some(NV2),
+    );
+
+    let matrix = RMatrix::new(vec![
+        Rc::new(RefCell::new(col0)),
+        Rc::new(RefCell::new(col1)),
+        Rc::new(RefCell::new(col2)),
+        Rc::new(RefCell::new(col3)),
+    ]);
+
+    matrix
+}
+
+//----------------------------------------------------------------
+
+// use graphics::{glyphs::ebgaramond::*, prelude::*};
+use notation_rs::{
+    prelude::*, render::fonts::ebgaramond::GLYPH_HEIGHT, types::some_cloneables::SomeCloneables,
+};
+// use render_notation::render::dev::*;
+
+pub fn r10() -> Vec<NRect> {
+    vec![NRect::new(0.0, 0.0, 10.0, 10.0)]
+}
+
+pub fn r20() -> Vec<NRect> {
+    vec![NRect::new(0.0, 0.0, 10.0, 20.0)]
+}
+
+// trait Col {}
+
+#[derive(Debug)]
+pub struct RItem {
+    pub rects: Vec<NRect>,
+    pub duration: Duration,
+    pub position: Position,
+    pub col_idx: usize,
+    pub row_idx: usize,
+}
+
+impl RItem {
+    pub fn new(rects: Vec<NRect>, dur: Duration) -> Self {
+        Self {
+            rects,
+            duration: dur,
+            position: 0,
+            col_idx: 0,
+            row_idx: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RCol {
+    pub duration: Duration,
+    pub rowitems: Vec<Option<Rc<RefCell<RItem>>>>,
+    pub spacing: f32,
+    pub col_idx: usize,
+}
+
+impl RCol {
+    pub fn new(rowitems: Vec<Option<Rc<RefCell<RItem>>>>, duration: Option<Duration>) -> Self {
+        Self {
+            rowitems,
+            duration: duration.unwrap_or(0),
+            spacing: 0.0,
+            col_idx: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RMatrix {
+    pub colitems: Vec<Rc<RefCell<RCol>>>,
+    pub rowitems: Vec<Vec<Option<Rc<RefCell<RItem>>>>>,
+}
+
+impl RMatrix {
+    pub fn new(colitems: Vec<Rc<RefCell<RCol>>>) -> Self {
+        let row_count = &colitems[0].borrow().rowitems.len();
+
+        let mut rowitems: Vec<Vec<Option<Rc<RefCell<RItem>>>>> = vec![vec![]; *row_count];
+
+        let firstcol = &colitems[0];
+        for item in firstcol.borrow().rowitems.iter() {
+            if item.is_none() {
+                panic!("firstcol has None - shouldn't have!");
+            }
+        }
+
+        let mut colidx = 0;
+        for col in &colitems {
+            let col: &RefCell<RCol> = col;
+
+            // set column index
+            col.borrow_mut().col_idx = colidx;
+
+            // check for rows integrity
+            if col.borrow().rowitems.len() != *row_count {
+                panic!("part_count mismatch");
+            }
+
+            let mut rowidx = 0;
+            for row in col.borrow_mut().rowitems.iter_mut() {
+                // set rowidx and colidx for item
+                if let Some(row) = row {
+                    let row: &RefCell<RItem> = row;
+                    row.borrow_mut().col_idx = colidx;
+                    row.borrow_mut().row_idx = rowidx;
+                }
+
+                // add cloned item to rowitems
+                let rowitem = if let Some(row) = row {
+                    let item = row.clone();
+                    Some(row.clone())
+                } else {
+                    None
+                };
+                rowitems[rowidx].push(rowitem);
+
+                rowidx += 1;
+            }
+
+            colidx += 1;
+        }
+        Self { colitems, rowitems }
+    }
+}
+
+fn main() {}
