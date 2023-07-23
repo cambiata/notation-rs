2ff798e5213dac73e271fe33d170b2637d70fa70
jonasnys@gmail.com <jonasnys@gmail.com>
2023-07-23 18:28:30 +0200

    update

--- examples/ex3.rs
+++ examples/ex3.rs
@@ -14,8 +14,12 @@ fn main() {
     println!("ex3");
 
     // let bar_data = QCode::bars("|clef G F | 0 0 / 0 0 0 ").unwrap();
-    let bar_data = QCode::bars("0 1").unwrap();
+    let bar_data =
+        // QCode::bars("0 nv8 1 n2 nv16 3 #0,-1 -2 -1 Nv8 -3 -4 % 2 b2 2 2 / nv4 0 0 0 0").unwrap();
+        QCode::bars("nv8 0 0 0 0 / 0 0").unwrap();
+
     // QCode::bars("|clefs G F - | 0 % 1 / 0 /lyr $lyr:aaa | 0 / 0 /lyr $lyr:bbb").unwrap();
+
     let (bartemplate, bars) = bar_data;
     let mut matrix = bars.to_matrix().unwrap();
 
