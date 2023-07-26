a724ce99238ae1717e69c62673ffbc9ecc44525e
jonasnys@gmail.com <jonasnys@gmail.com>
2023-07-26 21:54:35 +0200

    update

--- examples/ex3.rs
+++ examples/ex3.rs
@@ -36,7 +36,7 @@ fn main() {
     // let bar_data = QCode::bars("0 0 % nv8 3 5 5 3").unwrap();
 
     let bar_data = QCode::bars("nv16 4 3 5,3 1 nv8tri -3 -4,-6 -5 ").unwrap();
-    let bar_data = QCode::bars("-2 -2 % nv16 1 2 3 4 nv8 3 nv16 2 1 / nv8 0 2 4 1 |bl | nv16 -1 nv8 -2 nv16 -3 nv16 -4 nv8dot -2 % 2 2 / nv8 0 2  4 1 |bl | nv8 3 -2 -4 -6 / 0 2 ").unwrap();
+    let bar_data = QCode::bars("-2 -2 % nv16 1 #3 5 7 nv8 3 nv16 2 1 / nv8 0 2 4 1 |bl | nv16 -1 nv8 -2 nv16 -3 nv16 -4 nv8dot -2 % nv4dot 2 nv8 3 / nv8 0 2  4 1 |bl | nv8 3 p -4 -6 / 0 p ").unwrap();
 
     let (bartemplate, bars) = bar_data;
     let mut matrix = bars.to_matrix(&bartemplate).unwrap();
@@ -51,7 +51,7 @@ fn main() {
 
     matrix_to_svg(&matrix, "./examples/ex3A.svg");
 
-    // matrix.add_horizontal_space(100.0);
+    matrix.add_horizontal_space(100.0);
     matrix.add_vertical_space(50.0);
     matrix.calculate_col_row_item_measurements();
     matrix.calculate_matrix_size();
